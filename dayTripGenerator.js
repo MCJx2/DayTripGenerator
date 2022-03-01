b0126556e24d6d42d60199d640950e1069a467ee
unknown <montrae@montraejemison.com>
2022-03-01 16:59:35 -0600

    not sure about these user stories

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -65,7 +65,7 @@
 //   }
 // }
 
-//restaurantGenerator()
+// restaurantGenerator()
 
 
 
@@ -102,34 +102,71 @@
 //   transportationGenerator()
 
 
-  //array forms of entertainment
-let formsOfEntertainment = ["Movies", "Tubing", "Karoke", "City Tour"];
+//   //array forms of entertainment
+// let formsOfEntertainment = ["Movies", "Tubing", "Karoke", "City Tour"];
+
+// //function that takes in an array(myArray) and returns a random element from the array
+// //The "Math.random()" function is a built in Javascript function that "sorts through" an array, and picks a random element
+// //Since we want a value returned from this array, we MUST use the "return" keyword
+// function randomGenerator(myArray){
+//     let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
+//     return randomChoice
+// }
+
+// function entertainmentGenerator(){
+  
+//     let userIsHappy = false
+  
+//     while(userIsHappy === false){
+//         let randomEntertainment = randomGenerator(formsOfEntertainment)
+  
+//         let userResponse = prompt(`Your form of entertainment is ${randomEntertainment}. Are you ready for that?`)
+  
+//         if(userResponse.toLowerCase() === 'yes'){
+//           alert(`Enjoy your ${randomEntertainment}`)
+         
+//           userIsHappy = true
+//         }else if(userResponse.toLowerCase() === 'no'){
+//           userIsHappy = false
+//         }
+//     }
+//   }
+  
+//   entertainmentGenerator()
+
+
+
+  
+
+
+  //array dayTripGenerator
+
 
 //function that takes in an array(myArray) and returns a random element from the array
 //The "Math.random()" function is a built in Javascript function that "sorts through" an array, and picks a random element
 //Since we want a value returned from this array, we MUST use the "return" keyword
-function randomGenerator(myArray){
-    let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
-    return randomChoice
-}
+// function randomGenerator(myArray){
+//     let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
+//     return randomChoice
+//}
+let destination = []
+destination.push ["San Marcos", "Austin", "Kerrville", "New Braunfels"];
 
-function entertainmentGenerator(){
-  
-    let userIsHappy = false
-  
-    while(userIsHappy === false){
-        let randomEntertainment = randomGenerator(formsOfEntertainment)
-  
-        let userResponse = prompt(`Your form of entertainment is ${randomEntertainment}. Are you ready for that?`)
-  
-        if(userResponse.toLowerCase() === 'yes'){
-          alert(`Enjoy your ${randomEntertainment}`)
-         
-          userIsHappy = true
-        }else if(userResponse.toLowerCase() === 'no'){
-          userIsHappy = false
-        }
+function destinationChecker(destinationList){
+  let user_choice =  prompt("Select your destination")
+  for (let index = 0; index < destinationList.length; index++) {
+    if (destinationList[index] === user_choice){
+        console.log ("You found a destination")
     }
   }
-  
-  entertainmentGenerator()
+}
+destinationChecker(destination)
+
+// let restaurant = []
+// restaurant.push ["Chinese", "Italian", "Steak", "Vegan"];
+
+// let transportation = []
+// transportation.push ["Uber", "Train", "Car", "Bus"];
+
+// let entertainment
+// entertainment.push ["Movies", "Tubing", "Karoke", "City Tour"];
