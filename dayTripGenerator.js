aa88fff09a4c97592657a8bfcaffdf0c66124bfd
unknown <montrae@montraejemison.com>
2022-05-27 21:27:45 -0600

    Creating a single function

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,166 +1,113 @@
-// //console.log ("Hello World");
+ console.log ("Hello World");
 
-// //array of destinations
-// let listOfDestinations = ["San Marcos", "Austin", "Kerrville", "New Braunfels"];
-
-// //function that takes in an array(myArray) and returns a random element from the array
-// //The "Math.random()" function is a built in Javascript function that "sorts through" an array, and picks a random element
-// //Since we want a value returned from this array, we MUST use the "return" keyword
-// function randomGenerator(myArray){
-//     let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
-//     return randomChoice
-// }
-
-// function destinationGenerator(){
-//   // userIsHappy will determine if we continue looping through our "while" loop
-//   let userIsHappy = false
-//   // Since userIsHappy equals false by default, it will AUTOMATICALLY  enter into this while loop
-//   while(userIsHappy === false){
-//     // randomDestination will "catch" whatever value is RETURNED from the randomGenerator function
-//       let randomDestination = randomGenerator(listOfDestinations)
-
-//       let userResponse = prompt(`Your randomly generated destination is ${randomDestination}. Are you okay with that?`)
-
-//       if(userResponse.toLowerCase() === 'yes'){
-//         alert(`Enjoy your trip to ${randomDestination}`)
-//         // Since the "while" loop only runs if userIsHappy is false, as soon as it sees that its switched to "true", it will end the loop
-//         userIsHappy = true
-//       }else if(userResponse.toLowerCase() === 'no'){
-//         userIsHappy = false
-//       }
-//   }
-// }
-
-// destinationGenerator()
+//array of destinations
+let listOfDestinations = ["San Marcos", "Austin", "Kerrville", "New Braunfels"];
+let typesOfRestaurants = ["Chinese", "Italian", "Steak", "Vegan"];
+let modesOfTransportation = ["Uber", "Train", "Car", "Bus"];
+let formsOfEntertainment = ["Movies", "Tubing", "Karoke", "City Tour"];
 
 
-// //array of restaurants
-// let typesOfRestaurants = ["Chinese", "Italian", "Steak", "Vegan"];
+function randomGenerator(myArray){
+    let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
+    return randomChoice
+}
+function choiceGenerator(type, listOf){
+  let random;
+  // userIsHappy will determine if we continue looping through our "while" loop
+  let userIsHappy = false
+  // Since userIsHappy equals false by default, it will AUTOMATICALLY  enter into this while loop
+  while(userIsHappy === false){
+    // randomDestination will "catch" whatever value is RETURNED from the randomGenerator function
+      random = randomGenerator(listOf)
 
-// //function that takes in an array(myArray) and returns a random element from the array
-// //The "Math.random()" function is a built in Javascript function that "sorts through" an array, and picks a random element
-// //Since we want a value returned from this array, we MUST use the "return" keyword
-// function randomGenerator(myArray){
-//     let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
-//     return randomChoice
-// }
+      let userResponse = prompt(`Your randomly generated ${type} is ${random}. Are you okay with that?`)
+
+      if(userResponse.toLowerCase() === 'yes'){
+        alert(`Enjoy ${random}`)
+        // Since the "while" loop only runs if userIsHappy is false, as soon as it sees that its switched to "true", it will end the loop
+        userIsHappy = true
+      }else if(userResponse.toLowerCase() === 'no'){
+        userIsHappy = false
+      }
+  }
+  return random
+}
+let destinationChoice = choiceGenerator("destination", listOfDestinations)
+let restaurantChoice = choiceGenerator("restaurant", typesOfRestaurants)
+
+let randomDestination
+let randomRestaurant
+let randomTransportation
+let randomEntertainment
 
 
-// function restaurantGenerator(){
+
+
+//array of modes of transportation
+
+
+
+function transportationGenerator(){
   
-//   let userIsHappy = false
-
-//   while(userIsHappy === false){
-//       let randomRestaurant = randomGenerator(typesOfRestaurants)
-
-//       let userResponse = prompt(`Your randomly generated type of restaurant is ${randomRestaurant}. Are you okay with that?`)
-
-//       if(userResponse.toLowerCase() === 'yes'){
-//         alert(`Enjoy your dinner ${randomRestaurant}`)
-       
-//         userIsHappy = true
-//       }else if(userResponse.toLowerCase() === 'no'){
-//         userIsHappy = false
-//       }
-//   }
-// }
-
-// restaurantGenerator()
-
-
-
-// //array of modes of transportation
-// let modesOfTransportation = ["Uber", "Train", "Car", "Bus"];
-
-// //function that takes in an array(myArray) and returns a random element from the array
-// //The "Math.random()" function is a built in Javascript function that "sorts through" an array, and picks a random element
-// //Since we want a value returned from this array, we MUST use the "return" keyword
-// function randomGenerator(myArray){
-//     let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
-//     return randomChoice
-// }
-
-// function transportationGenerator(){
+    let userIsHappy = false
   
-//     let userIsHappy = false
+    while(userIsHappy === false){
+        randomTransportation = randomGenerator(modesOfTransportation)
   
-//     while(userIsHappy === false){
-//         let randomTransportation = randomGenerator(modesOfTransportation)
+        let userResponse = prompt(`Your mode of transportation is ${randomTransportation}. Are you okay with that?`)
   
-//         let userResponse = prompt(`Your mode of transportation is ${randomTransportation}. Are you okay with that?`)
-  
-//         if(userResponse.toLowerCase() === 'yes'){
-//           alert(`Enjoy your ${randomTransportation}`)
+        if(userResponse.toLowerCase() === 'yes'){
+          alert(`Enjoy your ${randomTransportation}`)
          
-//           userIsHappy = true
-//         }else if(userResponse.toLowerCase() === 'no'){
-//           userIsHappy = false
-//         }
-//     }
-//   }
+          userIsHappy = true
+        }else if(userResponse.toLowerCase() === 'no'){
+          userIsHappy = false
+        }
+    }
+  }
   
-//   transportationGenerator()
+  transportationGenerator()
 
 
-//   //array forms of entertainment
-// let formsOfEntertainment = ["Movies", "Tubing", "Karoke", "City Tour"];
+  //array forms of entertainment
 
-// //function that takes in an array(myArray) and returns a random element from the array
-// //The "Math.random()" function is a built in Javascript function that "sorts through" an array, and picks a random element
-// //Since we want a value returned from this array, we MUST use the "return" keyword
-// function randomGenerator(myArray){
-//     let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
-//     return randomChoice
-// }
 
-// function entertainmentGenerator(){
+
+function entertainmentGenerator(){
   
-//     let userIsHappy = false
+    let userIsHappy = false
   
-//     while(userIsHappy === false){
-//         let randomEntertainment = randomGenerator(formsOfEntertainment)
+    while(userIsHappy === false){
+        randomEntertainment = randomGenerator(formsOfEntertainment)
   
-//         let userResponse = prompt(`Your form of entertainment is ${randomEntertainment}. Are you ready for that?`)
+        let userResponse = prompt(`Your form of entertainment is ${randomEntertainment}. Are you ready for that?`)
   
-//         if(userResponse.toLowerCase() === 'yes'){
-//           alert(`Enjoy your ${randomEntertainment}`)
+        if(userResponse.toLowerCase() === 'yes'){
+          alert(`Enjoy your ${randomEntertainment}`)
          
-//           userIsHappy = true
-//         }else if(userResponse.toLowerCase() === 'no'){
-//           userIsHappy = false
-//         }
-//     }
-//   }
+          userIsHappy = true
+        }else if(userResponse.toLowerCase() === 'no'){
+          userIsHappy = false
+        }
+    }
+  }
   
-//   entertainmentGenerator()
+  entertainmentGenerator()
 
-
-
-  
-
-
+  console.log(`${randomDestination}/${randomRestaurant}/${randomTransportation}/${randomEntertainment}`)
   //array dayTripGenerator
 
 
-//function that takes in an array(myArray) and returns a random element from the array
-//The "Math.random()" function is a built in Javascript function that "sorts through" an array, and picks a random element
-//Since we want a value returned from this array, we MUST use the "return" keyword
-// function randomGenerator(myArray){
-//     let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
-//     return randomChoice
-//}
-let destination = []
-destination.push ["San Marcos", "Austin", "Kerrville", "New Braunfels"];
 
-function destinationChecker(destinationList){
-  let user_choice =  prompt("Select your destination")
-  for (let index = 0; index < destinationList.length; index++) {
-    if (destinationList[index] === user_choice){
-        console.log ("You found a destination")
-    }
-  }
-}
-destinationChecker(destination)
+// function destinationChecker(destinationList){
+//   let user_choice =  prompt("Select your destination")
+//   for (let index = 0; index < destinationList.length; index++) {
+//     if (destinationList[index] === user_choice){
+//         console.log ("You found a destination")
+//     }
+//   }
+// }
+// destinationChecker(destination)
 
 // let restaurant = []
 // restaurant.push ["Chinese", "Italian", "Steak", "Vegan"];
@@ -170,3 +117,6 @@ destinationChecker(destination)
 
 // let entertainment
 // entertainment.push ["Movies", "Tubing", "Karoke", "City Tour"];
+
+// let destination = []
+// destination.push ["San Marcos", "Austin", "Kerrville", "New Braunfels"];
